6b518463961590ca81592439e2f36bbbf185b7f2
Marypztr <nutrimary.77@gmail.com>
2019-04-30 16:16:48 -0500

    Empece a hacer las vistas y el back de create a new post

--- project2/routes/user.js
+++ project2/routes/user.js
@@ -10,4 +10,12 @@ router.get("/userData", (req,res,next) => {
   res.render("admin/contributions", {data})})
 })
 
+router.get("/createPost",(req,res,next) =>{
+res.render("user/newPost")
+})
+
+router.post("/createPost",(req,res,next) =>{
+  User.create
+})
+
 module.exports= router
